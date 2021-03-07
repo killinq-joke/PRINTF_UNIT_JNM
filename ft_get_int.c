83e7fc1969e09113c91dc32c9a92b17624f01f38
junior-one <gam-ing@live.fr>
2021-03-07 18:14:19 +0100

    ok

--- ft_get_int.c
+++ ft_get_int.c
@@ -12,19 +12,51 @@
 
 #include "ft_printf.h"
 
-char	*ft_int_flags(char **parsed_flags, va_list args)
+char	*ft_int_add_space(char *flag, char *itoa)
 {
-	char *end;
-	char *itoa;
-	int va_arg; 
-	/*va_arg = va_arg(args, int);
-	itoa = ft_itoa((long long int)va_arg);
-	if (!(end = ft_calloc(sizeof(end), ft_strlen(itoa) + 1)))
-		return (NULL);
-	ft_strcpy(end, itoa);
+	char	*temp;
+	int		space_nbr;
+	int 	total_size;
+	char	*end;
+
+	space_nbr = ft_atoi(flag);  
+	if (space_nbr > 0)
+	{
+		total_size = (space_nbr) - ft_strlen(itoa);
+		total_size < 0 ? total_size = 0 : total_size;
+		temp = ft_calloc(sizeof(temp), total_size + 1);
+		temp = ft_memset(temp, ' ', total_size);
+		end = ft_strjoin(temp, itoa);
+	}	
+	else
+	{	
+		total_size = (space_nbr * -1) - ft_strlen(itoa);
+		total_size < 0 ? total_size = 0 : total_size;
+		temp = ft_calloc(sizeof(temp), total_size + 1);
+		temp = ft_memset(temp, ' ', total_size);
+		end = ft_strjoin(itoa, temp);
+	}
+	free(temp);
 	free(itoa);
-	return (end);*/
-	/*attention ne marche pas avec les flags !!!!!*/
+	return (end);
+}
+
+char	*ft_int_flags(char **parsed_flags, char *itoa)
+{
+	int 	i;
+	int 	j;
+
+	i = 0;
+	j = 0;
+	while (parsed_flags[i] != 0)
+		i++;
+	while (j < i - 1)
+	{
+		itoa = ft_int_add_space(parsed_flags[j], itoa);
+		j++;
+	}	
+
+	return(itoa);
 }
 
 char	*ft_get_int(char *flags, va_list args, char **parsed_flags)
@@ -33,21 +65,10 @@ char	*ft_get_int(char *flags, va_list args, char **parsed_flags)
 	char *itoa;
 	int va_arg; 
 
+	va_arg = va_arg(args, int);
+	itoa = ft_itoa((long long int)va_arg);
 	
-	if (ft_strlen(flags) == 0) /* à modifier */
-	{
-		va_arg = va_arg(args, int);
-		itoa = ft_itoa((long long int)va_arg);
-		if (!(end = ft_calloc(sizeof(end), ft_strlen(itoa) + 1)))
-			return (NULL);
-		ft_strcpy(end, itoa);
-		free(itoa);
-		return (end);
-	}
-	else
-	{
-        end = ft_int_flags(parsed_flags, args);
-		return (end);
-	}
-	return (NULL);
+	if ((ft_strlen(flags))) 
+		itoa = ft_int_flags(parsed_flags, itoa);
+	return (itoa);
 }
