46eb3ac1cb652c50b1c66ce4d80924ce3c841af3
NotJustJoe <furienocturn@hotmail.fr>
2021-03-08 16:49:46 +0100

    -ft_get_str

--- ft_get_str.c
+++ ft_get_str.c
@@ -6,7 +6,7 @@
 /*   By: trofidal <trofidal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:13:14 by trofidal          #+#    #+#             */
-/*   Updated: 2021/03/06 22:26:40 by trofidal         ###   ########.fr       */
+/*   Updated: 2021/03/08 16:49:37 by trofidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,20 @@
 char    *ft_get_str(char *flags, va_list args)
 {
     char *end;
+    char *ptr;
     char *va_arg;
     
     if (ft_strlen(flags) == 0)
     {
         va_arg = va_arg(args, char *);
+        if (va_arg == NULL)
+        {
+            end = ft_calloc(sizeof(end), 7);
+            ptr = end;
+            end = ft_strjoin(end, "(null)");
+            free (ptr);
+            return (end);
+        }
         if (!(end = ft_calloc(sizeof(end), ft_strlen(va_arg) + 1)))
             return (NULL);
         ft_strcpy(end, va_arg);
