059129ff4bddce01dd088e851f0474009c87fa36
minikross <balta.olivier@gmail.com>
2021-03-13 16:24:51 +0100

    ft_get_g.c norme ok

--- ft_get_g.c
+++ ft_get_g.c
@@ -14,14 +14,15 @@
 
 static char	*ft_concat_string(double nbr)
 {
-	char *str;
-	char **tab;
-	
+	char		*str;
+	char		**tab;
+
 	str = ft_get_it_done(nbr);
 	tab = ft_split(str, 'e');
 	free(str);
 	tab[0][ft_strlen(tab[0]) - 1] = '\0';
-	if (!(str = ft_calloc(sizeof(char), ft_strlen(tab[0]) + ft_strlen(tab[1]) + 2)))
+	if (!(str = ft_calloc(sizeof(char), ft_strlen(tab[0]) + \
+	ft_strlen(tab[1]) + 2)))
 		return (NULL);
 	ft_strcpy(str, tab[0]);
 	str[ft_strlen(tab[0])] = 'e';
@@ -34,50 +35,52 @@ static char	*ft_concat_string(double nbr)
 
 static char	*ft_del_zero_end(char *str)
 {
-	int i;
-	int yes;
+	int	i;
+	int	yes;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] == '0')
+		if (str[i] == '0')
 		{
 			yes = 0;
-			while(str[i + yes] == '0')
+			while (str[i + yes] == '0')
 			{
 				if (str[yes + i + 1] == '\0')
 					str[i] = '\0';
 				yes++;
 			}
-			i+=yes;
+			i += yes;
 		}
 		i++;
 	}
 	return (str);
 }
 
-static int ft_get_n_len(size_t dup)
+static	int	ft_get_n_len(size_t dup)
 {
-	int len;
+	int	len;
+
 	len = 0;
-		while (dup > 0)
-		{
-			dup /= 10;
-			len++;
-		}
-		return (len);
+	while (dup > 0)
+	{
+		dup /= 10;
+		len++;
+	}
+	return (len);
 }
 
 char	*ft_get_g(char *flags, va_list args)
 {
-	char *flt;
-	double va_arg;
-	int len;
+	char		*flt;
+	double		va_arg;
+	int			len;
+
 	if (ft_strlen(flags) == 0)
 	{
 		va_arg = va_arg(args, double);
 		len = ft_get_n_len(va_arg);
-		if(len <= 6)
+		if (len <= 6)
 		{
 			flt = ft_putnbr_float(va_arg);
 			flt = ft_del_zero_end(flt);
