60131d1fddae66a1cbc649fff76a0813b6e78d04
minikross <balta.olivier@gmail.com>
2021-03-13 16:42:41 +0100

    ft_get_second_value.c norme ok

--- ft_get_second_value.c
+++ ft_get_second_value.c
@@ -12,26 +12,26 @@
 
 #include "ft_printf.h"
 
-int      ft_get_second_value(char *flags) // GET FLAGS SECOND VALUE
+int	ft_get_second_value(char *flags) // GET FLAGS SECOND VALUE
 {
-    int i;
-    int a;
-    char *second_value;
+	int			i;
+	int			a;
+	char		*second_value;
 
-    i = 0;
-    a = 0;
-    while (flags[i] != '.' && flags[i + 1] != '\0')
-        i++;
-    if (flags[i] == '.')
-        i++;
-    second_value = ft_calloc(sizeof(second_value), 10);
-    while (flags[i])
-    {
-        second_value[a] = flags[i];
-        a++;
-        i++;
-    }
-    a = ft_atoi(second_value);
-    free (second_value);
-    return (a);
+	i = 0;
+	a = 0;
+	while (flags[i] != '.' && flags[i + 1] != '\0')
+		i++;
+	if (flags[i] == '.')
+		i++;
+	second_value = ft_calloc(sizeof(second_value), 10);
+	while (flags[i])
+	{
+		second_value[a] = flags[i];
+		a++;
+		i++;
+	}
+	a = ft_atoi(second_value);
+	free (second_value);
+	return (a);
 }
