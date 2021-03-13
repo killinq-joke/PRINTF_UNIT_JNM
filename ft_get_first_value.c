d2ad5c197c34292a6e980d4676e41163c1d771c0
minikross <balta.olivier@gmail.com>
2021-03-13 14:58:23 +0100

    ft_get_first_value.c norme ok

--- ft_get_first_value.c
+++ ft_get_first_value.c
@@ -12,26 +12,26 @@
 
 #include "ft_printf.h"
 
-int      ft_get_first_value(char *flags) // GET FLAGS FIRST VALUE
+int	ft_get_first_value(char *flags)
 {
-    int i;
-    char *first_value;
+	int		i;
+	char	*first_value;
 
-    i = 0;
-    if (ft_strlen(flags) == 0)
-        return (i);
-    first_value = ft_calloc(sizeof(first_value), 10);
-    if (flags[0] == '.')
-        first_value[0] = '0';
-    else
-    {
-        while (flags[i] != '.' && flags[i] != '\0')
-        {
-            first_value[i] = flags[i];
-            i++;
-        }
-    }
-    i = ft_atoi(first_value);
-    free (first_value); //------
-    return (i);
+	i = 0;
+	if (ft_strlen(flags) == 0)
+		return (i);
+	first_value = ft_calloc(sizeof(first_value), 10);
+	if (flags[0] == '.')
+		first_value[0] = '0';
+	else
+	{
+		while (flags[i] != '.' && flags[i] != '\0')
+		{
+			first_value[i] = flags[i];
+			i++;
+		}
+	}
+	i = ft_atoi(first_value);
+	free(first_value);
+	return (i);
 }
