8fc319c23023735ec9b7f859a04412f7f3778f69
minikross <balta.olivier@gmail.com>
2021-03-13 14:54:05 +0100

    ft_get_e.c norme ok 95%

--- ft_get_e.c
+++ ft_get_e.c
@@ -14,9 +14,9 @@
 
 static long long int	ft_get_pow(long double nbr, int *is_pos)
 {
-	long long int i;
-	long long int count;
-	long long int result;
+	long long int			i;
+	long long int			count;
+	long long int			result;
 
 	result = 1;
 	count = 0;
@@ -35,17 +35,17 @@ static long long int	ft_get_pow(long double nbr, int *is_pos)
 		count++;
 	}
 	while (--count > 0)
-			result = result * 10; 
+		result = result * 10;
 	if (nbr < 1)
 		result = result / 10;
 	return (result);
 }
 
-static char		*ft_get_last(long long int pow, int is_pos)
+static	char			*ft_get_last(long long int pow, int is_pos)
 {
-	int i;
-	char *str;
-	char *itoa;
+	int		i;
+	char	*str;
+	char	*itoa;
 
 	if (!(str = ft_calloc(sizeof(str), 4)))
 		return (NULL);
@@ -72,14 +72,14 @@ static char		*ft_get_last(long long int pow, int is_pos)
 	}
 	else
 		str[3] = '0' + i;
-	free (itoa);
+	free(itoa);
 	return (str);
 }
 
-static double ft_get_round(double nbr)
+static	double			ft_get_round(double nbr)
 {
-	int count;
-	int is_neg;
+	int	count;
+	int	is_neg;
 
 	count = 0;
 	is_neg = 0;
@@ -93,48 +93,47 @@ static double ft_get_round(double nbr)
 	if ((int)nbr % 10 >= 5)
 	{
 		nbr = (nbr / 10) + 1;
-	} 
-	while (count > 0 && count--) 
+	}
+	while (count > 0 && count--)
 		nbr /= 10;
 	is_neg == 1 ? nbr = -nbr : nbr;
 	return (nbr);
 }
 
-char			*ft_get_it_done(double nbr)
+char					*ft_get_it_done(double nbr)
 {
-	char 	*end;
-	char	*final;
-	long long int 	pow; 
-	int is_pos;
+	char			*end;
+	char			*final;
+	long long int	pow;
+	int				is_pos;
 
 	is_pos = 0;
 	pow = ft_get_pow(nbr, &is_pos);
 	end = ft_get_last(pow, is_pos);
-
 	nbr = ft_get_round(nbr);
 	if (is_pos == 1)
 		nbr = nbr / pow;
-	else 
+	else
 		nbr = nbr * pow;
 	if (nbr < 1 && nbr > -1)
 		final = ft_putnbr_float(nbr * 10);
 	else
 		final = ft_putnbr_float(nbr);
 	final = ft_strcat(final, end);
-	free (end);
+	free(end);
 	return (final);
 }
 
-char	*ft_get_e(char *flags, va_list args)
+char					*ft_get_e(char *flags, va_list args)
 {
-	char *flt;
-	double va_arg;
+	char		*flt;
+	double		va_arg;
 
 	if (ft_strlen(flags) == 0)
 	{
 		va_arg = va_arg(args, double);
 		flt = ft_get_it_done(va_arg);
-		flt = ft_index(flt, flags); // TEST FOR FLAGS
+		flt = ft_index(flt, flags);
 		return (flt);
 	}
 	return (NULL);
