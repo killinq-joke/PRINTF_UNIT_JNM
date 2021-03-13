e41d9cfedaa84ca80b024101abb53e8a436ad634
minikross <balta.olivier@gmail.com>
2021-03-13 16:49:55 +0100

    ft_get_str.c norme ok mais a decomposer

--- ft_get_str.c
+++ ft_get_str.c
@@ -12,37 +12,37 @@
 
 #include "ft_printf.h"
 
-char            *ft_get_str(char *flags, va_list args)
+char	*ft_get_str(char *flags, va_list args)
 {
-    char *end; 
-    char *va_arg;
-    int a;
-    int b;
-  
-    b = 0;
-    if (strchr(flags, '.') != NULL)
-    {
-        a = ft_get_second_value(flags);
-        b++;
-    }
-    else
-        a = ft_get_first_value(flags);
-    va_arg = va_arg(args, char *);
-    if (va_arg == NULL && (((a >= 6 || a == 0) || a < 0) || b == 0))
-    {
-        end = ft_calloc(sizeof(end), 7); 
-        end = ft_strcpy(end, "(null)"); 
-    }
-    else if (va_arg == NULL && a < 6)
-    {
-        end = ft_strdup("");
-    }
-    else
-    {
-        if (!(end = ft_calloc(sizeof(end), ft_strlen(va_arg) + 1)))
-            return (NULL);
-        ft_strcpy(end, va_arg);
-    }
-    end = ft_index(end, flags); 
-    return (end);
+	char		*end;
+	char		*va_arg;
+	int			a;
+	int			b;
+
+	b = 0;
+	if (strchr(flags, '.') != NULL)
+	{
+		a = ft_get_second_value(flags);
+		b++;
+	}
+	else
+		a = ft_get_first_value(flags);
+	va_arg = va_arg(args, char *);
+	if (va_arg == NULL && (((a >= 6 || a == 0) || a < 0) || b == 0))
+	{
+		end = ft_calloc(sizeof(end), 7);
+		end = ft_strcpy(end, "(null)");
+	}
+	else if (va_arg == NULL && a < 6)
+	{
+		end = ft_strdup("");
+	}
+	else
+	{
+		if (!(end = ft_calloc(sizeof(end), ft_strlen(va_arg) + 1)))
+			return (NULL);
+		ft_strcpy(end, va_arg);
+	}
+	end = ft_index(end, flags);
+	return (end);
 }
